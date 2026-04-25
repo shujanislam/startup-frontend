2951d602b6ff36a460ef079cc53109cd895c20bb
ANUBHAVSINGH <singhanubhav7795@gmail.com>
2026-04-26 00:15:05 +0530

    added dashboard page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { Navigate } from 'react-router-dom'
 
 const IndexRoute = () => {
-  return <Navigate to="/login" replace />
+  return <Navigate to="/home" replace />
 }
 
 export default IndexRoute
